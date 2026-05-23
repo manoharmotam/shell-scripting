5f18720eb5ab5942ef43359780a63c6f3bb26345
manohar <manohar@gmail.com>
2026-05-23 17:10:12 +0000

    Assignment 2

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-MSG="Hello World"
+NAME=$1
+AGE=$2
+CITY=$3
+LIMIT=60
+CURRENT_AGE=$(($LIMIT-$2))
 
-echo $MSG
+echo "My Name is $1, I am $AGE years old and I live in $CITY."
+echo "Years until 60: $CURRENT_AGE"
