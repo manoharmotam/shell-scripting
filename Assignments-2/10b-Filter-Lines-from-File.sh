1703a7f181a025d1a4eb74a925c1085d78cb2c57
manohar <manohar@gmail.com>
2026-06-01 11:30:52 +0000

    All tasks

---
+++ Assignments-2/10b-Filter-Lines-from-File.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# FILE='data.csv'
+# OUTPUT_FILE='data.log'
+# HEADER=$(head -n1 "$FILE")
+
+# while IFS= read -r line;
+# do
+#     # echo "$HEADER"
+#     echo "$line" | grep -i inactive
+# done < $FILE
+
+
+
+#Incomplete Task
